264a29051594bacb91117b71e3e203affc05fd49
HaidDerAllerEchte <jonas.haid@hak-reutte.ac.at>
2024-02-26 16:02:32 +0100

    Aufgabe2

---
+++ src/arrays/Aufgabe2.java
@@ -0,0 +1,16 @@
+package arrays;
+
+public class Aufgabe2 {
+
+	public static boolean isProbablyApproaching(String[]signs) {
+		
+	}
+	
+	public static void main(String[] args) {
+		String[] signs1 = { "F", "DO", "MOS", "MOS", "MOS", "MOS", "WES"};
+		System.out.println(isProbablyApproaching(signs1));
+		
+		
+	}
+
+}
